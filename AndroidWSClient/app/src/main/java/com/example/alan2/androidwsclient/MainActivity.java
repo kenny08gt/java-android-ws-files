9e047ecd5a70112d0b16e2b15c7b3e5585e5d494
kenny08gt <alan220193@hotmail.com>
2016-04-23 19:49:54 -0600

    sin metodo pasar archivo

--- AndroidWSClient/app/src/main/java/com/example/alan2/androidwsclient/MainActivity.java
+++ AndroidWSClient/app/src/main/java/com/example/alan2/androidwsclient/MainActivity.java
@@ -39,10 +39,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AsyncCallWS task = new AsyncCallWS();
-                task.execute();
-                //calculate();
-
+               Toast.makeText(MainActivity.this,"Descargar",Toast.LENGTH_SHORT).show();
             }
         });
         findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
@@ -53,11 +50,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    private class AsyncCallWS extends AsyncTask<Void, Void, Void> {
+
+    private class AsyncCallWSSumar extends AsyncTask<Void, Void, Void> {
         @Override
         protected Void doInBackground(Void... params) {
             Log.i(TAG, "doInBackground");
-            descargar_archivo();
+            sumar();
             return null;
         }
 
@@ -87,96 +85,6 @@ public class MainActivity extends AppCompatActivity {
             Log.i(TAG, "onProgressUpdate");
         }
 
-    }
-    private class AsyncCallWSSumar extends AsyncTask<Void, Void, Void> {
-        @Override
-        protected Void doInBackground(Void... params) {
-            Log.i(TAG, "doInBackground");
-            sumar();
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            Log.i(TAG, "onPostExecute");
-
-            EditText et=(EditText)findViewById(R.id.editText3);
-            et.setText(resultado + "");
-        }
-
-        @Override
-        protected void onPreExecute() {
-            Log.i(TAG, "onPreExecute");
-        }
-
-        @Override
-        protected void onProgressUpdate(Void... values) {
-            Log.i(TAG, "onProgressUpdate");
-        }
-
-    }
-    public void descargar_archivo(){
-        String NAMESPACE = "http://servicios/";
-        String URL = "http://192.168.153.1:8080/ws_glash/?wsws1dl";
-        String METHOD_NAME = "Descargar";
-        String SOAP_ACTION = "http://servicios/Descargar";
-
-
-
-        try
-        {
-            SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
-            //EditText txt1=(EditText)findViewById(R.id.editText);
-            //EditText txt2=(EditText)findViewById(R.id.editText2);
-            //int uno=Integer.parseInt(txt1.getText().toString());
-            //int dos=Integer.parseInt(txt2.getText().toString());
-            request.addProperty("parameter", 1);
-
-
-            SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
-            envelope.setOutputSoapObject(request);
-            //envelope.dotNet = true;
-
-            HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
-            androidHttpTransport.call(SOAP_ACTION, envelope);
-
-            //SoapPrimitive result=(SoapPrimitive) envelope.getResponse();
-
-            //Toast.makeText(MainActivity.this,result.toString(),Toast.LENGTH_SHORT).show();
-            // Get the result
-                    SoapPrimitive resultsRequestSOAP = (SoapPrimitive) envelope.getResponse();
-                    byte[] result = Base64.decode(resultsRequestSOAP.toString());
-
-                    // Save the file
-                    try
-                    {
-                        File createfile = new File(Environment.getExternalStorageDirectory(),"/Folder/");
-                        createfile.mkdirs();
-                        File outputFile = new File(createfile,"cancion.mp3");//creating temporary file in phone Memory
-                        new FileOutputStream(outputFile);
-                        File filepath = new File(Environment.getExternalStorageDirectory(),"/Folder/cancion.mp3");
-                        OutputStream pdffos = new FileOutputStream(filepath);
-                        pdffos.write(result);//writing the binary string into the payslip.pdf temporary file
-                        pdffos.flush();
-                        pdffos.close();
-                        resultado="exito";
-                    }
-                    catch(FileNotFoundException ex)
-                    {
-                        System.out.println("FileNotFoundException : " + ex);
-                        resultado=ex.toString();
-                    }
-                    catch(IOException ioe)
-                    {
-                        System.out.println("IOException : " + ioe);
-                        resultado=ioe.toString();
-                    }
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-
-        }
     }
     public void sumar(){
         String NAMESPACE = "http://servicios/";
